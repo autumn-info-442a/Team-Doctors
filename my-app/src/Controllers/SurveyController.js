61f5bd6aa805805ae8e007ace430cbc48734f180
Vivian Hua <vivianhua@Vivians-MacBook-Pro.local>
2020-11-09 15:46:52 -0800

    add function stubs

---
+++ my-app/src/Controllers/SurveyController.js
@@ -0,0 +1,45 @@
+const { ReactComponent } = require("*.svg");
+
+class SurveyController extends ReactComponent.Component {
+    
+    function goNextSurveyQuestion(response) {
+
+    }
+    
+    function goBackSurveyQuestion() {
+
+    }
+
+    function submitSurvey() {
+
+    }
+
+    function submitSurvey() {
+
+    }
+
+    function getCurrentQuestion() {
+
+    }
+
+    function getSurveyResponse(surveyQuestion) {
+
+    }
+
+    function getSurveyResponses() {
+
+    }
+
+    function getTestingCenters() {
+        
+    }
+
+    function computeResults() {
+        
+    }
+
+
+    render() {
+        return <div></div>
+    }
+}
