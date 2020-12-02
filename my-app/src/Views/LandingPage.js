c07f20b1a7c1ee3f1f24ff3c01e6f6c0e6dfc150
ISABELLA_ABULHOSN <ABULHOSN@uw.edu>
2020-12-02 10:59:10 -0800

    more css updates a new logo

--- my-app/src/Views/LandingPage.js
+++ my-app/src/Views/LandingPage.js
@@ -11,6 +11,9 @@ class LandingPage extends Component {
             <h1>Where2Test</h1>
             <p className="Descr">Fill out a few questions to find out where the closest COVID-19 testing center is that best fits your needs.</p>
             <button onClick={this.props.startSurvey} type="button" className="Start-btn">START</button>
+            <footer>
+            <div>Icons made by <a href="https://smashicons.com/" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+            </footer>
         </div>
       );
   }
--- my-app/src/Views/ResultsPage.js
+++ my-app/src/Views/ResultsPage.js
@@ -59,7 +59,7 @@ class ResultsPage extends Component {
             <div>
                 <p className="Result-intro">Here are the testing centers you should go to based on your selections:</p>
                 <p className="Result-explanation">They are filtered based on your preferences and ordered based on closest location.</p>
-                <p className="Result-explanation">Testing center information last updated on: {this.props.lastUpdated[0]}</p>
+                <p className="Result-explanation last-updated">Testing center information last updated on: <span className="updated-date">{this.props.lastUpdated[0]}</span></p>
             </div>
             <div>
               {resultsDisplay}
