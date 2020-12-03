ed0e4eedec820246b444636c41051b34dddaf6d1
ISABELLA_ABULHOSN <ABULHOSN@uw.edu>
2020-12-02 16:59:16 -0800

    updated css for hyperlinks and edited landing page

--- my-app/src/Views/LandingPage.js
+++ my-app/src/Views/LandingPage.js
@@ -9,7 +9,8 @@ class LandingPage extends Component {
       return (
         <div>
             <h1>Where2Test</h1>
-            <p className="Descr">Fill out a few questions to find out where the closest COVID-19 testing center is that best fits your needs.</p>
+            <p className="Descr">We have created this web app to make finding a COVID-19 testing center easier and more accessible for local Seattle people. Please fill out a few questions to find out where the closest COVID-19 testing center is that best fits your needs.</p>
+            <p className="Descr disclaimer">At this moment, this app can only locate testing centers in the Seattle area, and we only support valid Washington addresses.</p>
             <button onClick={this.props.startSurvey} type="button" className="Start-btn">START</button>
             <footer>
             <div>Icons made by <a href="https://smashicons.com/" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
